48a957fbf19791a265a194c32546ada001f86811
Teki Chan <tekichan@gmail.com>
2019-01-28 23:47:29 +0800

    Add Image Capture Input

---
+++ src/dict-ez/ImageUtils.js
@@ -0,0 +1,74 @@
+export function base64ArrayBuffer(arrayBuffer) {
+    var base64    = ''
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    var bytes         = new Uint8Array(arrayBuffer)
+    var byteLength    = bytes.byteLength
+    var byteRemainder = byteLength % 3
+    var mainLength    = byteLength - byteRemainder
+
+    var a, b, c, d
+    var chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
+}
+  
+export function getOrientation(buffer) {
+    var view = new DataView(buffer);
+    if (view.getUint16(0, false) != 0xFFD8) return -2;
+    var length = view.byteLength, offset = 2;
+    while (offset < length) {
+    var marker = view.getUint16(offset, false);
+    offset += 2;
+    if (marker == 0xFFE1) {
+        if (view.getUint32(offset += 2, false) != 0x45786966) return -1;
+        var little = view.getUint16(offset += 6, false) == 0x4949;
+        offset += view.getUint32(offset + 4, little);
+        var tags = view.getUint16(offset, little);
+        offset += 2;
+        for (var i = 0; i < tags; i++)
+        if (view.getUint16(offset + (i * 12), little) == 0x0112)
+            return view.getUint16(offset + (i * 12) + 8, little);
+    }
+    else if ((marker & 0xFF00) != 0xFF00) break;
+    else offset += view.getUint16(offset, false);
+    }
+    return (-1);
+}
--- src/dict-ez/OcrPage.js
+++ src/dict-ez/OcrPage.js
@@ -1,10 +1,136 @@
 import React, { Component}  from 'react';
-import { Page } from 'react-onsenui';
+import { Page, Toolbar, BottomToolbar, Segment, ToolbarButton } from 'react-onsenui';
+import { base64ArrayBuffer, getOrientation } from './ImageUtils.js';
+const [ LANG_NONE, LANG_ZH_HK, LANG_ZH_CN, LANG_EN ] = [ "", "zh-hk", "zh-tw", "en-gb" ];
 
 class OcrPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lang: LANG_NONE
+            , scannedFileBase64: ''
+        }
+        this.handleReset = this.handleReset.bind(this);
+        this.handleCapturing = this.handleCapturing.bind(this);
+        this.clickLangBtn = this.clickLangBtn.bind(this);
+    }
+
+    getBase64(file) {
+        var self = this;
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = function () {
+            var srcOrientation = getOrientation(reader.result);
+            var base64 = 'data:image/jpg;base64,' + base64ArrayBuffer(reader.result);
+            var img = new Image();
+            img.setAttribute("src", base64);
+            img.onload = function() {
+                var width = 800;
+                var height = Math.floor(img.height * (width / img.width));
+                var canvas = document.createElement('canvas');
+                canvas.width = width;
+                canvas.height = height;
+                var ctx = canvas.getContext("2d");
+    
+                // set proper canvas dimensions before transform & export
+                if ([5,6,7,8].indexOf(srcOrientation) > -1) {
+                    canvas.width = height;
+                    canvas.height = width;
+                } else {
+                    canvas.width = width;
+                    canvas.height = height;
+                }
+    
+                // transform context before drawing image
+                switch (srcOrientation) {
+                    case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+                    case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
+                    case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
+                    case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+                    case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+                    case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+                    case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+                    default: ctx.transform(1, 0, 0, 1, 0, 0);
+                }
+    
+                ctx.drawImage(img, 0, 0, width, height);
+                self.setState({
+                    scannedFileBase64: canvas.toDataURL("image/jpeg",0.7)
+                });
+            }
+        };
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+    }
+    
+    resetAll() {
+        this.setState({
+            lang: LANG_NONE
+            , scannedFileBase64: ''
+        });
+    }
+
+    handleReset(_event) {
+        this.resetAll();
+    }
+
+    /**
+     * Capture Camera Input Event
+     * @param {} _event 
+     */
+    handleCapturing(_event) {
+        this.getBase64(_event.target.files[0]);
+    }
+
+    clickLangBtn(_lang, _event) {
+        this.setState({
+            lang: _lang
+        })
+        document.getElementById("imgScanned").click()
+    }
+
     render() {
         return(
-<Page>OcrPage</Page>
+<Page>
+    <Toolbar>
+        <div className="center">
+默書易 Dict-Ez：先選擇語言
+        </div>
+        <div className="right">
+            <ToolbarButton onClick={this.handleReset}><i className="zmdi zmdi-refresh"></i></ToolbarButton>
+        </div>
+    </Toolbar>
+    <div>
+        <input type="file" id="imgScanned" name="imgScanned" accept="image/*" capture="camera" 
+            style={{display:'none'}}
+            onChange={this.handleCapturing}
+            onClick={
+                (event) => {
+                    console.log('onClick'); 
+                    event.target.value = null;
+                }
+            } 
+        />        
+{ this.state.lang === LANG_NONE &&
+    <img src="./images/education_bg.jpg" alt="Please select a language" height="100%" />
+}
+{ this.state.lang !== LANG_NONE && this.state.scannedFileBase64 && 
+    <img src={ this.state.scannedFileBase64 } alt="Optical Character Recognition" height="100%" />
+}
+    </div>
+    <BottomToolbar modifier="material">
+        <Segment style={{width: '100%'}} modifier="material">
+            <ToolbarButton 
+                onClick={
+                    (_event) => {this.clickLangBtn(LANG_ZH_HK, _event)}
+                }
+            >廣東話</ToolbarButton>
+            <ToolbarButton>普通語（繁）</ToolbarButton>
+            <ToolbarButton>English</ToolbarButton>
+        </Segment>     
+    </BottomToolbar>    
+</Page>
         );
     }
 }
