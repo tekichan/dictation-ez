e0b78ac394a0c154028ade9cb57bf0263e27f17b
Teki Chan <tekichan@gmail.com>
2019-01-30 23:54:02 +0800

    initiate TTS page

--- src/dict-ez/TtsPage.js
+++ src/dict-ez/TtsPage.js
@@ -1,10 +1,87 @@
 import React, { Component}  from 'react';
-import { Page } from 'react-onsenui';
+import { Page, Toolbar, ToolbarButton, List, ListItem, Range, Row, Col } from 'react-onsenui';
+
+const TOOLBAR_MSG_INIT = '按此播放';
 
 class TtsPage extends Component {
+    constructor(props) {
+        super(props);
+        if (this.props.ttsContent) {
+            // Split Content Text with newline
+            this.state = {
+                toolbar_msg: TOOLBAR_MSG_INIT
+                , ttsContents: this.props.ttsContent.trim().split(/\r?\n/)
+                , speechSpeed: 0
+            };
+        } else {
+            this.state = {
+                toolbar_msg: TOOLBAR_MSG_INIT
+                , ttsContents: []
+                , speechSpeed: 0
+            };
+        }
+
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    resetAll() {
+        if (this.props.ttsContent) {
+            // Split Content Text with newline
+            this.setState({
+                ttsContents: this.props.ttsContent.trim().split(/\r?\n/)
+                , speechSpeed: 0
+            });
+        } else {
+            this.setState({
+                ttsContents: []
+                , speechSpeed: 0
+            });
+        }        
+    }
+
+    handleReset(_event) {
+        this.resetAll();
+    }
+
     render() {
         return(
-<Page>TTS</Page>
+<Page>
+    <Toolbar>
+        <div className="center">
+{ this.state.toolbar_msg }<i className="zmdi zmdi-play-circle"></i>
+        </div>
+        <div className="right">
+            <ToolbarButton onClick={this.handleReset}><i className="zmdi zmdi-refresh"></i></ToolbarButton>
+        </div>
+    </Toolbar>
+    <Row>
+        <Col width="20%">
+            <i class="zmdi zmdi-audio"></i>
+        </Col>
+        <Col width="60^">
+        <Range modifier="material"
+                style={{width: "100%"}}
+                value={this.state.speechSpeed}
+                onChange={(event) => this.setState({speechSpeed: parseInt(event.target.value)})}
+        />
+        </Col>
+        <Col width="20%">
+            <i class="zmdi zmdi-audio"></i>
+        </Col>
+    </Row>
+    <Row><Col>
+        <List
+            dataSource={ this.state.ttsContents }
+            renderRow={(row, idx) => (
+                <ListItem modifier={idx === this.state.ttsContents.length - 1 ? 'longdivider' : null}>
+                <div class="left"><i className="zmdi zmdi-play-circle"></i></div>
+                <div class="center">{row}</div>
+                <div class="right"><i className="zmdi zmdi-edit"></i></div>
+                </ListItem>
+            )}
+        />
+    </Col></Row>
+</Page>
         );
     }
 }
